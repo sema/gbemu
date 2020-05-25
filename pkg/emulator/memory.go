f8465aad87662c752d62d801416fda2a7468bafe
Casper Svenning Jensen <caspersj@uber.com>
2020-05-25 10:43:07 +0200

    Refactor ROM in preparation for MBC support

--- pkg/emulator/memory.go
+++ pkg/emulator/memory.go
@@ -19,46 +19,6 @@ type memoryPage interface {
 	String() string
 }
 
-type rom struct {
-	data []byte
-}
-
-func newROM() *rom {
-	return &rom{
-		data: make([]byte, bytes32k),
-	}
-}
-
-func (r *rom) Read8(address uint16) byte {
-	// as the ROM is placed at the beginning of the address space we don't need to offset the input address
-	return r.data[address]
-}
-
-func (r *rom) Write8(address uint16, v byte) {
-	// TODO write only allowed for MBC
-	notImplemented("writes to MBC not implemented")
-}
-
-func (r *rom) String() string {
-	return "ROM"
-}
-
-func (r *rom) LoadROM(path string) error {
-	log.Printf("loading ROM at %s", path)
-
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	} else if len(data) != bytes32k {
-		return fmt.Errorf("invalid ROM size: expected ROM to contain %d bytes but contained %d bytes", bytes32k, len(data))
-	}
-
-	r.data = data
-
-	log.Printf("Loaded %d bytes from ROM", len(data))
-	return nil
-}
-
 type bootROM struct {
 	data []byte
 }
